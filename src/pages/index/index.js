2ffc78b896f3265b1d45a9ab963d7515a7dea73d
ChenMou <1106417223@qq.com>
2019-08-10 19:16:52 +0800

    add:右键添加/移除旗子

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -8,6 +8,7 @@ class Index extends React.Component {
         this.state = {
             row: 10,
             column: 10,
+            mineCount: 10,
             table: [[]]
         }
     }
@@ -18,9 +19,11 @@ class Index extends React.Component {
         for (let i = 0; i < table.length; i++) {
             for (let j = 0; j < table[i].length; j++) {
                 let obj = Object.create(null);
+
                 obj.selected = false;
-                
                 obj.number = 0;
+                obj.hasFlag = false;
+
                 table[i][j] = obj;
             }
         }
@@ -29,35 +32,49 @@ class Index extends React.Component {
         }, () => { console.log(this.state.table) });
     }
 
-    onClick (row, column) {
+    onMouseUp (row, column, e) {
         console.log(row, column);
         let temp = this.state.table;
-        temp[row][column].selected = true;
+        if (e.button === 2) {
+            if (temp[row][column].selected === false) {
+                temp[row][column].hasFlag = !temp[row][column].hasFlag;
+            }
+        } else {
+            if (temp[row][column].hasFlag === false) {
+                temp[row][column].selected = true;
+            }
+        }
+        
         this.setState({
             table: temp
-        }, () => { console.log(this.state.table[row]) })
+        })
     }
 
     render () {
         let mineSweeperRow = this.state.table.map((row, rowIndex) => {
             return (
-                <div className='mine-sweeper-row' key={'row' + rowIndex}>
-                    {
-                        row.map((item, columnIndex) => {
-                            return (
-                                <div 
-                                    className={
-                                        this.state.table[rowIndex][columnIndex].selected 
-                                        ? 'mine-sweeper-item mine-sweeper-item-selected' 
-                                        : 'mine-sweeper-item'
-                                    } 
-                                    key={'row' + rowIndex + '-column' + columnIndex}
-                                    onClick={() => this.onClick(rowIndex, columnIndex)}
-                                >
-                                </div>
-                            )
-                        })
-                    }
+                <div onContextMenu={(e) => {e.preventDefault()}}>
+                    <div className='mine-sweeper-row' key={'row' + rowIndex}>
+                        {
+                            row.map((item, columnIndex) => {
+                                return (
+                                    <div 
+                                        className={
+                                            item.selected 
+                                            ? 'mine-sweeper-item mine-sweeper-item-selected' 
+                                            : 'mine-sweeper-item'
+                                        } 
+                                        key={'row' + rowIndex + '-column' + columnIndex}
+                                        onMouseUp={(e) => this.onMouseUp(rowIndex, columnIndex, e)}
+                                    >
+                                        {
+                                            item.hasFlag ? '🚩' : null
+                                        }
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             )
         })
